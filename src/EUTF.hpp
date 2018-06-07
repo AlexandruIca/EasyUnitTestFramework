564e5c5b75ca114376f40f0ac8ae60113576f2e7
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2018-06-07 19:52:37 +0100

    Made the framework header only. Refactored a lot of the code to make it cleaner and easier to implement stuff like xml output and json output in the future.

--- src/EUTF.hpp
+++ src/EUTF.hpp
@@ -2,8 +2,6 @@
 #ifndef EUTF_HPP
 #define EUTF_HPP
 
-#include <string>
-
 //
 // * Where to print information regarding tests
 //
@@ -18,13 +16,15 @@
 #define EUTF_CONCAT(a, b) EUTF_PASTE(a, b)
 
 //
-// * 'name' can can be anything
+// * Give the test some tags that have to be quoted strings
 //
-#define EUTF_NEW_TEST(name) \
+// * The first tag is considered the name of the test, the rest are there to differentiate tests with the same names
+//
+#define EUTF_NEW_TEST(...) \
 struct EUTF_CONCAT(EUTF_Test_, __LINE__) : public eutf::Test \
 { \
 	EUTF_CONCAT(EUTF_Test_, __LINE__)() \
-		: eutf::Test(this, std::string{ eutf::TestName() + #name }) \
+		: eutf::Test(std::string{ eutf::TestSuiteName() }, std::list<const char*>{ __VA_ARGS__ }) \
 	{} \
 	\
 	void run() override; \
@@ -40,7 +40,8 @@ void EUTF_CONCAT(EUTF_Test_, __LINE__)::run()
 if(!(P)) \
 {\
 	this->OnFatal(EUTF_LOG, __FILE__, __LINE__, #P);\
-	return;\
+	\
+	return; \
 }
 
 //
@@ -60,21 +61,23 @@ if(!(P)) \
 #define EUTF_MESSAGE(msg) this->OnMessage(EUTF_LOG, __FILE__, __LINE__, msg)
 
 //
-// * 'name' can be anything
+// * 'name' has to be a quoted string
 //
 #define EUTF_TEST_SUITE(name) \
 static int EUTF_CONCAT(eutf_test_suite_, __LINE__) = []() -> int \
-{\
-	eutf::AddTestName(#name);\
-	return 0;\
-}();\
+{ \
+	eutf::AddTestSuiteName(name); \
+	\
+	return 0; \
+}(); \
 namespace 
 
 #define EUTF_END_TEST_SUITE() \
 static int EUTF_CONCAT(eutf_test_suite_end_, __LINE__) = []() -> int \
 { \
-	eutf::DeleteTestName();\
-	return 0;\
+	eutf::DeleteTestSuiteName(); \
+	\
+	return 0; \
 }()
 
 //
@@ -90,37 +93,179 @@ static int EUTF_CONCAT(eutf_tests_results, __LINE__) = []() -> int \
 	return 0; \
 }()
 
+#include <string>
+#include <list>
+
 namespace eutf {
 	
 	enum Log : int
 	{
-		CONSOLE //, XML in the future
+		CONSOLE //, XML in the future, maybe even json
 	};
 
 	struct Test
 	{
 	protected:
-		std::string m_name;
+		std::string m_suite;
+		std::list<const char*> m_tags;
 		
 		Test() = delete;
-		Test(Test* test, std::string&& name);
+		Test(std::string&& suite, std::list<const char*>&& tags);
 		virtual ~Test();
 
-		virtual void OnFatal(int log, const char* file, int line, const char* callstr);
-		virtual void OnFailure(int log, const char* file, int line, const char* callstr);
-		virtual void OnWarning(int log, const char* file, int line, const char* callstr);
-		virtual void OnMessage(int log, const char* file, int line, const char* msg);
+		void OnFatal(int log, const char* file, int line, const char* callstr);
+		void OnFailure(int log, const char* file, int line, const char* callstr);
+		void OnWarning(int log, const char* file, int line, const char* callstr);
+		void OnMessage(int log, const char* file, int line, const char* msg);
 	
 	public:
 		virtual void run() = 0;
 	};
 
-	std::string TestName();
-	void AddTestName(const char* m_name);
-	void DeleteTestName();
+	std::string TestSuiteName();
+	void AddTestSuiteName(const char* m_name);
+	void DeleteTestSuiteName();
 
 	void RunAll(int log);
 
 }
 
-#endif
+#ifdef EUTF_MAIN
+
+#include <iostream>
+#include <vector>
+#include <utility>
+
+namespace eutf {
+	std::vector<std::string> name;
+	std::size_t warnings = 0;
+	std::size_t errors = 0;
+	std::size_t fatal_errors = 0;
+
+	std::vector<Test*>& get_tests_queue()
+	{
+		static std::vector<Test*> tests_queue;
+		return tests_queue;
+	}
+}
+
+eutf::Test::Test(std::string&& suite, std::list<const char*>&& tags)
+	: m_suite(std::move(suite))
+	, m_tags(std::move(tags))
+{
+	eutf::get_tests_queue().push_back(this);
+}
+
+eutf::Test::~Test() {}
+
+std::string eutf::TestSuiteName()
+{
+	std::string final_name;
+
+	for (const auto& str : eutf::name) {
+		final_name += str;
+		final_name += "\\";
+	}
+
+	return final_name;
+}
+
+void eutf::AddTestSuiteName(const char* m_name)
+{
+	eutf::name.push_back(std::string{ m_name });
+}
+
+void eutf::DeleteTestSuiteName()
+{
+	eutf::name.pop_back();
+}
+
+static void RunTests(std::ostream& f)
+{
+	std::size_t n = eutf::get_tests_queue().size();
+
+	if (n == 1) {
+		f << "1 test" << std::endl;
+	}
+	else {
+		f << n << " tests" << std::endl;
+	}
+
+	for (const auto& test : eutf::get_tests_queue()) {
+		test->run();
+	}
+
+	f << std::endl;
+
+	if (eutf::warnings) {
+		f << "Warnings: " << eutf::warnings << std::endl;
+	}
+	else {
+		f << "***No warnings" << std::endl;
+	}
+	if (eutf::errors) {
+		f << "Failures: " << eutf::errors << std::endl;
+	}
+	else {
+		f << "***No failures" << std::endl;
+	}
+	if (eutf::fatal_errors) {
+		f << "Fatal errors: " << eutf::fatal_errors << std::endl;
+	}
+	else {
+		f << "***No fatal errors";
+	}
+}
+
+void eutf::RunAll(int log)
+{
+	switch (log)
+	{
+	case eutf::CONSOLE: {
+		RunTests(std::cout);
+		break;
+	}
+	}
+}
+
+static void PrintInfo(int log, const std::string& name, const std::list<const char*>& tags, const char* type, const char* file, int line, const char* str)
+{
+	switch (log)
+	{
+	case eutf::CONSOLE: {
+		std::cout << file << "(" << line << ") " << name;
+
+		for (const auto& tag : tags)
+			std::cout << "{" << tag << "} ";
+
+		std::cout << " [" << type << "]: " << str << std::endl;
+	}
+	}
+}
+
+void eutf::Test::OnFatal(int log, const char* file, int line, const char* callstr)
+{
+	++fatal_errors;
+	PrintInfo(log, this->m_suite, this->m_tags, "FATAL", file, line, callstr);
+}
+
+void eutf::Test::OnFailure(int log, const char* file, int line, const char* callstr)
+{
+	++errors;
+	PrintInfo(log, this->m_suite, this->m_tags, "FAILURE", file, line, callstr);
+}
+
+void eutf::Test::OnWarning(int log, const char* file, int line, const char* callstr)
+{
+	++warnings;
+	PrintInfo(log, this->m_suite, this->m_tags, "WARNING", file, line, callstr);
+}
+
+void eutf::Test::OnMessage(int log, const char* file, int line, const char* msg)
+{
+	PrintInfo(log, this->m_suite, this->m_tags, "MESSAGE", file, line, msg);
+}
+
+#endif // EUTF_MAIN
+
+#endif // !ETUF_HPP
